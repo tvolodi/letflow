d587ca596888d5da40b436340ab89c09e7aa54c5
Vladimir Titenko <tvolodi@gmail.com>
2026-08-31 14:40:38 +0500

    fix(REQ-199): rename migrations 000001/000002 to 050001/050002 to avoid REQ-203 timestamp collision

--- lib/letflow/tenant_provisioning.ex
+++ lib/letflow/tenant_provisioning.ex
@@ -473,10 +473,10 @@ defmodule Letflow.TenantProvisioning do
      "20260830040001_create_alert_trigger_state.exs"},
     {20_260_830_040_002, Letflow.Repo.Migrations.CreateAlertHookEmissionState,
      "20260830040002_create_alert_hook_emission_state.exs"},
-    {20_260_831_000_001, Letflow.Repo.Migrations.CreateEffectCompletions,
-     "20260831000001_create_effect_completions.exs"},
-    {20_260_831_000_002, Letflow.Repo.Migrations.CreateCorrelationCursors,
-     "20260831000002_create_correlation_cursors.exs"}
+    {20_260_831_050_001, Letflow.Repo.Migrations.CreateEffectCompletions,
+     "20260831050001_create_effect_completions.exs"},
+    {20_260_831_050_002, Letflow.Repo.Migrations.CreateCorrelationCursors,
+     "20260831050002_create_correlation_cursors.exs"}
   ]
 
   @doc """
